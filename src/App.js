f7d76acd0dbcb8278ac6de81fd31702456a30e92
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-03 21:03:14 +0100

    create some tags with jsx and style them and use props (propreties) assign the value to Tweet

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Tweet from './Tweet';
 
 function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
-      <Tweet />
+    <div className="app">
+      <Tweet name="Bogdan" message= "This is a random tweet"/>
+      <Tweet name="john Snow" message= "I am the true King"/>
+      <Tweet name="winter is coming" message= "700k my dudes"/>
+      <Tweet name="mosh" message= "My new course"/>
     </div>
   );
 }
